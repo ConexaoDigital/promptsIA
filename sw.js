0e2cf3f88f6b352238961252fd467c2c2d20fc81
ConexaoDigital <luizzasilva2022@gmail.com>
2026-02-26 16:16:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,18 @@
-const CACHE_NAME = 'ebooks-cache-v1';
+const CACHE_NAME = 'ebooks-cache-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/',
+  'index.html',
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
